aa247454f29de9c5480267296ccec4800514518b
mohammadmoghimi <mmoghimi2003@gmail.com>
2026-08-25 19:29:56 +0430

    Add fetch website list

--- frontend/src/app/pages/websites/websites.ts
+++ frontend/src/app/pages/websites/websites.ts
@@ -5,7 +5,7 @@ import {
   ReactiveFormsModule,
   Validators
 } from '@angular/forms';
-import { WebsiteService } from '../../services/website';
+import { Website, WebsiteService } from '../../services/website';
 
 @Component({
   selector: 'app-websites',
@@ -15,56 +15,84 @@ import { WebsiteService } from '../../services/website';
   styleUrl: './websites.scss'
 })
 export class Websites implements OnInit {
+  websiteForm!: FormGroup;
+  websites: Website[] = [];
   isCrawling = false;
+  isLoading = true;
   errorMessage = '';
   successMessage = '';
-  websiteForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private websiteService: WebsiteService
   ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.websiteForm = this.formBuilder.group({
       name: ['', Validators.required],
       url: ['', [Validators.required, Validators.pattern(/^https?:\/\/.+/)]],
       description: ['']
     });
+
+    this.loadWebsites();
   }
 
   submit() {
-    console.log('SUBMIT');
-
     if (this.websiteForm.invalid || this.isCrawling) {
       this.websiteForm.markAllAsTouched();
       return;
     }
 
     const { name, url, description } = this.websiteForm.getRawValue();
-    this.processCrawl(name!, url!, description || '');
+    this.processCrawl(name, url, description || '');
   }
 
-  private processCrawl(name: string, url: string, description: string) {
+  private loadWebsites() {
+    this.websiteService.getWebsites().subscribe({
+      next: websites => {
+        console.log(websites,'websites');
+        
+        this.websites = websites;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.errorMessage = 'خطا در دریافت وب‌سایت‌ها.';
+        this.isLoading = false;
+        console.log(Error , 'error');
+        
+      }
+    });
+  }
+
+  private processCrawl(
+    name: string,
+    url: string,
+    description: string
+  ) {
     this.isCrawling = true;
     this.errorMessage = '';
     this.successMessage = '';
 
-    console.log('SENDING REQUEST');
-    this.websiteService
-      .crawlWebsite(name, url, description)
-      .subscribe({
-        next: response => {
-          this.isCrawling = false;
-          this.successMessage =
-            `وب‌سایت با موفقیت پردازش شد. ${response.crawl.pages_processed} صفحه پردازش شد.`;
-          this.websiteForm.reset();
-        },
-        error: error => {
-          this.isCrawling = false;
-          this.errorMessage =
-            error.error?.error || 'خطا در پردازش وب‌سایت.';
-        }
-      });
+    this.websiteService.crawlWebsite(name, url, description).subscribe({
+      next: response => {
+        this.isCrawling = false;
+        this.successMessage =
+          `وب‌سایت با موفقیت پردازش شد. ${response.crawl.pages_processed} صفحه پردازش شد.`;
+
+        this.websiteForm.reset();
+        this.loadWebsites();
+      },
+      error: error => {
+        this.isCrawling = false;
+        this.errorMessage =
+          error.error?.error || 'خطا در پردازش وب‌سایت.';
+      }
+    });
+  }
+
+  truncateUrl(url: string): string {
+    const maxLength = 40;
+    if (!url) return '';
+    return url.length > maxLength ? url.substring(0, maxLength) + '...' : url;
   }
 }
