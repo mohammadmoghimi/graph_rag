71d7fe381f3c82a4361296f5ea625909ffd74a54
mohammadmoghimi <mmoghimi2003@gmail.com>
2026-08-27 12:13:48 +0430

    Add style to ativated chat

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { Auth } from '../../services/auth';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ChatService, ChatSession } from '../../services/chat';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,6 +14,7 @@ export class Dashboard {
   isSidebarCollapsed = false;
   isChatsExpanded = false;
   chats = signal<ChatSession[]>([]);
+  activeChatId = signal<number | null>(null);
 
   toggleSidebar() {
     this.isSidebarCollapsed = !this.isSidebarCollapsed;
@@ -21,11 +23,20 @@ export class Dashboard {
   constructor(
     private authService: Auth,
     private router: Router,
-    private chatService: ChatService
+    private chatService: ChatService,
+    private route: ActivatedRoute
   ) {}
   
   ngOnInit() {
     this.loadChats();
+
+    this.setActiveChatFromRoute();
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.setActiveChatFromRoute();
+      });
   }
 
   loadChats() {
@@ -35,7 +46,17 @@ export class Dashboard {
   }
 
   openChat(id: number) {
+    this.activeChatId.set(id);
     this.router.navigate(['/chats', id]);
+  }
+
+  private setActiveChatFromRoute() {
+    const id = this.route.firstChild?.snapshot.paramMap.get('id');
+    if (id) {
+      this.activeChatId.set(Number(id));
+    } else {
+      this.activeChatId.set(null);
+    }
   }
 
   toggleChats() {
